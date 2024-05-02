ec435a978bee2319f2676dcde095227aa1df0142
Alfonso Miguel Hernández <99462404+FonzHdz@users.noreply.github.com>
2024-05-02 01:32:04 -0500

    OAuth branch

--- pagos/src/main/java/com/segurosTuPapa/pagos/client/UsuarioClient.java
+++
@@ -1,7 +0,0 @@
-package com.segurosTuPapa.pagos.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "usuarios", url = "http://localhost:8090")
-public interface UsuarioClient {
-}
